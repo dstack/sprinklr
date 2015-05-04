13038424a99b2a7d41b1dc96284b3d4448dbee94
Drew Strickland <andrew.flagship@gmail.com>
2015-05-04 11:43:28 -0600

    incremental

--- index.js
+++ index.js
@@ -11,62 +11,52 @@ var state = ['off', 'off', 'off', 'off'];
 
 // need to spin up the bridge exe right here
 
-function createBridge(){
-  var bridge = cp.spawn('bin/digiusb.exe',[], {stdio: 'inherit'});
-  bridge.on('error', function(msg){
-    console.log('err', arguments);
-  });
-  bridge.on('exit', function(msg){
-    console.log('exit', arguments);
-  });
-  bridge.on('close', function(msg){
-    console.log('close', arguments);
-  });
-  bridge.on('disconnect', function(msg){
-    console.log('disc', arguments);
-  });
-  bridge.on('message', function(msg){
-    console.log('msg', arguments);
-  });
+function USBSend(msg){
+  cp.exec(' bin\\send.exe ' + msg);
 }
 
-var bridgeUSB =createBridge();
 
-function turnOnOffZone(z, on){
-  var zoneIdx = zone - 1,
+function turnOnOffZone(z, cb, on){
+  var zoneIdx = z - 1,
     set = on? starts : stops;
   if(set[zoneIdx]){
     // send set[zoneIdx];
+    USBSend(set[zoneIdx]);
     state[zoneIdx] = on? 'on' : 'off';
-    return true;
+    cb(true);
   }
   else{
-    return false;
+    cb(false);
   }
 }
 
 var app = connect()
-  app.use(quip);
   app.use(require('serve-static')(__dirname + '/public'));
   app.use(dispatch({
-    '/start/:zone': function(req,res,next,zone){
-      var done = turnOnOffZone(zone, true);
-      if(done){
-        res.ok('zone ' + zone + ' started');
-      }
-      else{
-        res.error('Zone '+zone+' not supported');
-      }
+    'GET /start/:zone': function(req,res,next,zone){
+      turnOnOffZone(zone, function(done){
+        if(done){
+          res.writeHead(200);
+          res.end('zone ' + zone + ' started');
+        }
+        else{
+          res.writeHead(400);
+          res.end('Zone '+zone+' not supported');
+        }
+      }, true);
     },
-    '/stop/:zone':  function(req,res,next,zone){
-      var done = turnOnOffZone(zone);
-      if(done){
-        res.ok('zone ' + zone + ' stopped');
-      }
-      else{
-        res.error('Zone '+zone+' not supported');
-      }
-    },
+    'GET /stop/:zone':  function(req,res,next,zone){
+      turnOnOffZone(zone, function(done){
+        if(done){
+          res.writeHead(200);
+          res.end('zone ' + zone + ' stopped');
+        }
+        else{
+          res.writeHead(400);
+          res.end('Zone '+zone+' not supported');
+        }
+      });
+    }
   }));
 
 var server = http.createServer(app).listen(5280);
